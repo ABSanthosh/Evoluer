9a518e9c5a545ebd191ec610b9a62d2049e19a2b
ABSanthosh <a.b.santhosh02@gmail.com>
2023-08-10 04:10:34 +0530

    chore: removed useless stuff

--- Misc/src/components/Hello/hello.js
+++
@@ -1,9 +0,0 @@
-import { Tonic } from "../../../node_modules/@socketsupply/tonic/dist/tonic.min.js";
-
-class MyGreeting extends Tonic {
-  render() {
-    return this.html`<div>Hello, World.</div>`;
-  }
-}
-
-Tonic.add(MyGreeting, "my-greeting");
